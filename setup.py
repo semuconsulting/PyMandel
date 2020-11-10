0d1c7db32f7bedebdb40fa6d983d5a29a3e36213
semuadmin <semuadmin@users.noreply.github.com>
2020-11-10 13:41:10 +0000

    travis.yml updated

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Desktop Environment',
         'Topic :: Education',
