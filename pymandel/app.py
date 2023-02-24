b34dab6fb0cdb2153058f771938795528f62327e
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2023-02-24 23:15:26 +0000

    Update app.py

--- pymandel/app.py
+++ pymandel/app.py
@@ -3,7 +3,19 @@ Mandelbrot generator - Main tkinter application class
 
 Created on 29 Mar 2020
 
-@author: semuadmin
+:author: semuadmin
+:copyright: SEMU Consulting © 2020
+:license: GPL3
+
+This file is part of PyMandel.
+
+PyMandel is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+PyMandel is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>. 
 """
 
 import os
